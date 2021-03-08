7d309fe8dede1542a9adf3ba9b07609948cf7cea
Cecilia Lee <leececilia14@gmail.com>
2021-03-08 00:51:21 -0500

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here!
+
+
+
+
+
+document.querySelector("main#main").remove();
+
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory"
+newHeader.innerHTML = "Ceci is the champion!";
+newHeader.className = "victory";
